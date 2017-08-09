23b6cb8eae50d62be44db8062a5a06b8e36e9988
Guangxian Piao <guangxian.piao@aexp.com>
2017-08-09 14:30:00 -0700

    update Examples and About page verbiage

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,8 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h2>About Component</h2>
+            <h2>About</h2>
+            <p>Welcome to about Page</p>
         </div>
     );
 };
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,8 @@ var React = require('react');
 var Examples = (props) => {
     return (
         <div>
-            <h2>About Examples</h2>
+            <h2>Examples</h2>
+            <p>Welcome to Examples page</p>
         </div>
     );
 };
