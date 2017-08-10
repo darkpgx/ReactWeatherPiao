a2e9db19b20cca9b9298c3fdbf7b5d90be4fc18c
Larry Piao <darkpgx@gmail.com>
2017-08-09 22:02:29 -0700

    Add grid style & include static links for examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,12 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
-    return (
-        <div>
-            <h2>Examples</h2>
-            <p>Welcome to Examples page</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to="/?location=Hayward">Hayward, CA</Link>
+        </li>
+        <li>
+          <Link to="/?Philadelphia">Philadelphia, PA</Link>
+        </li>
+      </ol>
+    </div>
+  );
 };
 
 module.exports = Examples;
