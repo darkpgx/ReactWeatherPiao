4d1e18f2efe31ac376d3ff7a2ade30123f0290b0
Larry Piao <darkpgx@gmail.com>
2017-08-09 22:15:45 -0700

    Style about page & add resource links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,12 +1,21 @@
 var React = require('react');
 
 var About = (props) => {
-    return (
-        <div>
-            <h2>About</h2>
-            <p>Welcome to about Page</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is a weather application built on React.</p>
+      <p>Here are some of the tools I used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+        </li>
+        <li>
+          <a href="https://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 module.exports = About;
