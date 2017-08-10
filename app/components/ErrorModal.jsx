6030a6c6d9a8caf99936ad79fdb1eef9eacf5529
Guangxian Piao <guangxian.piao@aexp.com>
2017-08-10 14:58:11 -0700

    fix error modal bug

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
   getDefaultProps: function() {
@@ -12,12 +14,8 @@ var ErrorModal = React.createClass({
   },
   //Need the following lifeCycle method to mount modal
   componentDidMount: function() {
-    var modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();
-  },
-  render: function() {
     var {title, message} = this.props;
-    return (
+    var modalMarkup = (
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
         <h4>{title}</h4>
         <p>{message}</p>
@@ -27,6 +25,20 @@ var ErrorModal = React.createClass({
           </button>
         </p>
       </div>
+    );
+
+    //The following code is added as react doesn't work too well with third party module such as Foundation
+    //This will change jsx markup to string version
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    //Use ReactDOM.findDOMNode to locate current div and change html to modal
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+  render: function() {
+    return(
+      <div></div>
     );
   }
 });
